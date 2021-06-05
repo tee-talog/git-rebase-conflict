c013211fcf2e44d84545abbd51879f0cdeee7ad4
talog <tee.talog@gmail.com>
2021-06-05 12:55:15 +0900

    ユーザー一覧を返すエンドポイントを追加

--- router.js
+++ router.js
@@ -1,9 +1,20 @@
+const users = [
+  { id: 1, name: 'user 1' },
+  { id: 2, name: 'user 2' },
+  { id: 3, name: 'user 3' },
+]
+
 const router = (pathname) => {
   switch (pathname) {
     case '/':
       return {
         statusCode: 200,
         responseData: 'Hello World',
+      }
+    case '/users':
+      return {
+        statusCode: 200,
+        responseData: JSON.stringify(users),
       }
     default:
       return {
