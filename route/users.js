79fe7435e86d75abe1885236026513f4401d3cd5
talog <tee.talog@gmail.com>
2021-06-05 13:02:03 +0900

    ユーザー一覧を個別ファイルに定義し、それを返すエンドポイントを追加

---
+++ route/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: 'user 1', gender: 1, group: 'A' },
+  { id: 2, name: 'user 2', gender: 2, group: 'A' },
+  { id: 3, name: 'user 3', gender: 3, group: 'B' },
+]
+
+module.exports = JSON.stringify(users)
--- router.js
+++ router.js
@@ -1,9 +1,16 @@
+const users = require('./users')
+
 const router = (pathname) => {
   switch (pathname) {
     case '/':
       return {
         statusCode: 200,
         responseData: 'Hello World',
+      }
+    case '/users':
+      return {
+        statusCode: 200,
+        responseData: users,
       }
     default:
       return {
