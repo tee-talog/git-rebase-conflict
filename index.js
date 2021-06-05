d2a66e90e842cf63d797bcaebf5061486ada9c61
talog <tee.talog@gmail.com>
2021-06-05 12:55:29 +0900

    リクエストを受けたときにログを出力する

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const router = require('./router')
 
 const server = http.createServer((req, res) => {
+  console.log('receive request')
+
   const { statusCode, responseData } = router(req.url)
   res.statusCode = statusCode
   res.end(responseData)
